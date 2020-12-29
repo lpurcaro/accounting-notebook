3056a1b39123b50e4dc037675bcb119b9c4e8353
lucila.purcaro <lucila.purcaro@despegar.com>
2020-12-29 19:28:35 -0300

    validate transactionId and throw specific error

--- server/api/components/transactions/controller.ts
+++ server/api/components/transactions/controller.ts
@@ -2,10 +2,10 @@ import * as TransactionService from './service';
 import * as BalanceService from '../balance/service';
 
 import Transaction from './model';
-import { transactionNotFound } from "../../../errors/apiErrors";
+import { transactionNotFound, invalidIdSupplied } from '../../../errors/apiErrors';
 
 export const getTransactions = async (req, res) => {
-    const transactions = await TransactionService.getAll()
+    const transactions = await TransactionService.getAll();
     res.json({ transactions })
 }
 
@@ -19,7 +19,13 @@ export const addTransaction = async (req, res) => {
 }
 
 export const getTransactionById = async (req, res) => {
-    const transaction = await TransactionService.getById(req.params.transactionId);
+    const transactionId = req.params.transactionId;
+
+    if (!transactionId || isNaN(transactionId)) {
+        throw invalidIdSupplied(transactionId)
+    }
+
+    const transaction = await TransactionService.getById(transactionId);
 
     if (!transaction) {
         throw transactionNotFound(req.params.transactionId)
--- server/errors/apiErrors.ts
+++ server/errors/apiErrors.ts
@@ -15,3 +15,9 @@ export const insufficientFunds = (funds: number) =>{
     const errMsg = `Insufficient funds: ${funds}`;
     return new ApiError(errMsg, ErrorType.INVALID_ARGS);
 };
+
+export const invalidIdSupplied = (id: any) => {
+    const errMsg = `Invalid ID supplied: ${id}`;
+    return new ApiError(errMsg, ErrorType.INVALID_ARGS);
+}
+
